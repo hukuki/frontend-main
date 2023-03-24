a81c37f279314dba8b6b7dbaf64396c00993d247
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-03-25 00:59:02 +0300

    Implemented only the CSS of the pages. TODO: Separate the components and add functionality

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,17 +1,41 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 // Components
-import { Flex } from "@/components/base/layout";
-import FilterMenu from "./FilterMenu";
-import SearchResults from "./SearchResults";
+import { GoLaw } from "react-icons/go";
+import { Icon } from "@chakra-ui/icons";
+import { Box, Flex } from "@/components/base/layout";
+import { SearchBar } from "@/components/common";
 
-const Search = () => {
-  return (
-    <Flex height="100%" width="100%" gap={5} padding="40px">
-      <FilterMenu />
-      <SearchResults />
+// CSS
+import styles from "./SearchPage.module.css"
+
+const SearchPage = () => {
+  const router = useRouter();
+    return (
+    <>
+      <div className={styles.container}>
+        <div className={styles["logo__container"]}>
+          <h1 className={styles.logo}>DeepLex</h1>
+        </div>
+        <form action="" className={styles["searchbar__form"]}>
+          <div className={styles["searchbar__container"]}>
+            <input className={styles["searchbar__input"]} placeholder="Döküman ara" />
+          </div>
+          <button className={styles["search__button"]} type="submit">Search</button>
+        </form>
+      </div>
+      {/*
+    <Flex height="100%" align="center" direction="column" justify="center" gap="10px" paddingBottom="100px">
+      <Icon as={GoLaw} color="dark.quinary" fontSize={200} />
+      <Box w="50%" minW="500px" maxW="750px">
+        <SearchBar onSubmit={(search, category) => router.push(`/search-results?category=${category}&search=${search}`)} />
+      </Box>
     </Flex>
+    */}
+    </>
   );
 };
 
-export default Search;
+export default SearchPage;
