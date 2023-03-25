5414c82470ea0e032227b14641df138b367e3c12
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-03-25 17:32:15 +0300

    Changed the color theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -9,11 +9,16 @@ import { Input } from "./components/forms";
 // Colors
 const colors = {
   dark: {
-    primary: "#25274D",
-    secondary: "#E9EBEE",
-    tertiary: "#C4C4C4",
-    quaternary: "#4E005E",
-    quinary: "#00BFBF",
+    50: "#DEF4FF",
+    100: "#B0DDFF",
+    200: "#82C5FC",
+    300: "#52ADF7",
+    400: "#2596F4",
+    500: "#0B7DDA",
+    600: "#0061AB",
+    700: "#00457B",
+    800: "#002A4D",
+    900: "#000F1F",
   },
 };
 
