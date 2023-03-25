86a2d92f7a0657282fb874f1faf4b999d0e45f72
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-03-25 23:17:38 +0300

    Firebase dummy trial

---
+++ src/utils/initAuth.js
@@ -0,0 +1,47 @@
+import { init } from 'next-firebase-auth'
+
+const initAuth = () => {
+  init({
+    authPageURL: '/auth',
+    appPageURL: '/',
+    loginAPIEndpoint: '/api/login',
+    logoutAPIEndpoint: '/api/logout',
+    onLoginRequestError: (err) => {
+      console.error(err)
+    },
+    onLogoutRequestError: (err) => {
+      console.error(err)
+    },
+    // Use application default credentials (takes precedence over firebaseAdminInitConfig if set)
+    // useFirebaseAdminDefaultCredential: true,
+    firebaseClientInitConfig: {
+      apiKey: process.env.FIREBASE_API_KEY, // required
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+    },
+    cookies: {
+      name: '_deepLex', // required
+      // Keys are required unless you set `signed` to `false`.
+      // The keys cannot be accessible on the client side.
+      keys: [
+        process.env.COOKIE_SECRET_CURRENT,
+        process.env.COOKIE_SECRET_PREVIOUS,
+      ],
+      httpOnly: true,
+      maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
+      overwrite: true,
+      path: '/',
+      sameSite: 'strict',
+      secure: true, // set this to false in local (non-HTTPS) development
+      signed: true,
+    },
+    onVerifyTokenError: (err) => {
+      console.error(err)
+    },
+    onTokenRefreshError: (err) => {
+      console.error(err)
+    },
+  })
+}
+
+export default initAuth
