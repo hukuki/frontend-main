4bce9c2718c2aacc340ad331c52bb428f99010b4
onurerenarpaci <oarpaci18@ku.edu.tr>
2023-04-05 20:48:01 +0300

    comment out logout for now

--- src/pages/api/logout.js
+++ src/pages/api/logout.js
@@ -1,14 +1,14 @@
-import { unsetAuthCookies } from 'next-firebase-auth';
-import initAuth from '../../utils/initAuth';
+// import { unsetAuthCookies } from 'next-firebase-auth';
+// import initAuth from '../../utils/initAuth';
 
-initAuth();
+// initAuth();
 
 const handler = async (req, res) => {
-  try {
-    await unsetAuthCookies(req, res);
-  } catch (e) {
-    return res.status(500).json({ error: 'Unexpected error.' });
-  }
+  // try {
+  //   await unsetAuthCookies(req, res);
+  // } catch (e) {
+  //   return res.status(500).json({ error: 'Unexpected error.' });
+  // }
   return res.status(200).json({ success: true });
 };
 
