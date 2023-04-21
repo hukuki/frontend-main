c9e5d48d21cd0726a330f25d09cd0bd8302fb2ef
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-04-21 22:49:32 +0300

    Started the functionality of search results page mostly on Filter options

--- src/components/common/filter-checkbox/FilterCheckbox.js
+++ src/components/common/filter-checkbox/FilterCheckbox.js
@@ -3,13 +3,18 @@ import React, { useState } from 'react';
 import styles from './FilterCheckbox.module.css';
 import { CheckIcon } from '@chakra-ui/icons';
 
-export const FilterCheckbox = ({ label, isChecked, ...props }) => {
+export const FilterCheckbox = ({ label, isChecked, onChecked, ...props }) => {
   const defaultChecked = isChecked || false;
   const [checked, setChecked] = useState(defaultChecked);
 
+  const handleClick = () => {
+    onChecked();
+    setChecked((prev) => !prev);
+  };
+
   return (
     <div className={styles['container']}>
-      <div className={styles['input-container']} onClick={() => setChecked((prev) => !prev)}>
+      <div className={styles['input-container']} onClick={handleClick}>
         <input className={checked && styles['checked']} type="checkbox" checked={checked} />
         {checked && <CheckIcon color="primary.900" className={styles['check-icon']} />}
       </div>
--- src/components/common/filter-input/FilterInput.js
+++ src/components/common/filter-input/FilterInput.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import styles from './FilterInput.module.css';
 
-export const FilterInput = ({ type, placeholder, className, ...props }) => {
-  const [value, setValue] = useState(null);
-
-  const handleInputChange = (e) => {
-    setValue(e.target.value);
-  };
-
+export const FilterInput = ({ type, value, placeholder, onChange, className, onSubmit, ...props }) => {
   return (
     <div className={className}>
-      <input className={styles['input']} type={type} placeholder={placeholder} onChange={handleInputChange} value={value} />
+      <input
+        className={styles['input']}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') onSubmit(value);
+        }}
+      />
     </div>
   );
 };
